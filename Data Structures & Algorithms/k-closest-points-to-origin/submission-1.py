aec91d39f1f19958dea86cddcb1ae4fbe434bb31
Lily Seika Chisholm <75229642+lilychisholm@users.noreply.github.com>
2026-09-25 20:44:26 -0400

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        #need to use a min heap to track k minimum distances to origin
+        #can we do heapify based on an item in an array? that way we could have each point with its distance to the origin, and use the distance to heapify off of
+        #take entire list of [point, distance to origin], heapify by distance, then pop until length of heap is k
+        #ok looked it up, python automatically compares tuples y their first element! put everything in a tuple with the distance to origin as the first element, then the point as the second element
+        #the minimum will be at the top, so we can keep popping and adding popped elements to the final array until the array is of length k. this will give us the k points of minimum distance
+        for i in range(len(points)):
+            points[i] = ((points[i][0]**2 + points[i][1]**2), points[i])
+        heapq.heapify(points)
+
+        final = []
+        while len(final) < k:
+            final.append(heapq.heappop(points)[1])
+
+        return final
+
+
+        
