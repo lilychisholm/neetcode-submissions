6e7b43de93d84004d45f63acb1e0c634b9da2979
Lily Seika Chisholm <75229642+lilychisholm@users.noreply.github.com>
2026-09-25 20:59:42 -0400

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        #two methods in mind: max heap, min heap
+        #max heap: heapify, then pop until we've popped k elements
+        #min heap: keep a min heap of length k
+        #loop through the nums list, adding them to a final heap
+        #once the list has reached a length of k, start popping the minimum element each time a new one is added
+        #we will be left with a heap of the k largest elements, where the minimum of the k largest is at the top, aka our kth largest element
+        
+        #min heap approach:
+        heapq.heapify(nums)
+        while len(nums) > k:
+            heapq.heappop(nums)
+
+        return nums[0]
+        
